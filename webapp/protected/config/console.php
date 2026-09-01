151db44d9fd9a20abc3b469c5c8bf52f170f619c
Miguel Montaño <miiguel2m@gmail.com>
2026-09-01 12:47:32 +0200

    feat console.php and yiic.php added to the project

---
+++ webapp/protected/config/console.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Configuracion de la aplicacion de consola (yiic).
+ * Mismo import/db que main.php; sin urlManager porque no aplica
+ * fuera de peticiones web.
+ */
+return array(
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'Yii Bank Console',
+
+    'import' => array(
+        'application.models.*',
+        'application.interfaces.*',
+        'application.repositories.*',
+        'application.services.*',
+        'application.services.InterestCalculationStrategy.*',
+    ),
+
+    'components' => array(
+        'db' => array_merge(
+            array('class' => 'CDbConnection'),
+            require(dirname(__FILE__) . '/database.php')
+        ),
+    ),
+);
