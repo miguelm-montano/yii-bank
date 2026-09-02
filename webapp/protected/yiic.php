ccaef2d613c15632e2c9d912d2124ffdb1847c33
Miguel Montaño <miiguel2m@gmail.com>
2026-09-02 12:50:23 +0200

    fix(error-reporting): suppress E_DEPRECATED for PHP 8.5 compatibility)

--- webapp/protected/yiic.php
+++ webapp/protected/yiic.php
@@ -1,5 +1,8 @@
 <?php
 
+// Suppress PHP 8.5+ E_DEPRECATED for Yii 1.1 legacy casts compatibility
+error_reporting(E_ALL & ~E_DEPRECATED);
+
 $yiic = dirname(__FILE__) . '/../vendor/yiisoft/yii/framework/yiic.php';
 $config = dirname(__FILE__) . '/config/console.php';
 
